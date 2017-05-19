e7b1ea2202c498cee1e1dd6491615734b0714afb
Quốc Quyền Lục <lucquocquyen@gmail.com>
2017-05-20 00:04:08 +0300

    add MSE evaluation

--- mean_shift_runner.py
+++ mean_shift_runner.py
@@ -4,21 +4,17 @@ import math
 import  pdb
 from sklearn.cluster import estimate_bandwidth
 import matplotlib.pyplot as plt
+from mean_shift_utils import get_centroids,mse
 
-def plot_solution(X,gt):
+def plot_solution(c,gt,data):
     plt.figure()
-    # cx = np.array([c.xy_ for c in sol.centroids_])
-    # for c in sol.centroids_:
-    #   partition = sol.get_partition(c.label_)
-    #   pxy = np.array([[p.getX(), p.getY()] for p in partition])
-    #   # pdb.set_trace()
-    #   plt.scatter(pxy[:, 0], pxy[:, 1], s=2, marker='.', c='b')
-    plt.scatter(X[:, 0], X[:, 1], marker='.', c='r')
+    plt.scatter(c[:, 0], c[:, 1], marker='.', c='r')
+    plt.scatter(data[:, 0], data[:, 1], marker='.', c='g')
     plt.scatter(gt[:, 0], gt[:, 1], marker='.', c='m', )
     plt.show()
 
-def ci(sol,gt,k):
-    cens = np.array([c.xy_ for c in sol.centroids_])
+def ci(cens,gt,k):
+    # cens = np.array([c.xy_ for c in sol.centroids_])
     return max(one_way_ci(cens,gt,k),one_way_ci(gt,cens,k))
 
 def one_way_ci(cens_A, cens_B, n_clus):
@@ -31,6 +27,7 @@ def one_way_ci(cens_A, cens_B, n_clus):
         mapTo = dist.index(min(dist))
         orphans[mapTo]=0;
     return sum(orphans)
+
 
 def main(arg):
     if arg=='full':
@@ -55,29 +52,17 @@ def main(arg):
         data = np.loadtxt(fi)
         gt = np.loadtxt(fi_gt)
         reference_points = data
+        # pdb.set_trace()
         mean_shifter = ms.MeanShift()
-        kernel_bandwidth = look_distance
+        # kernel_bandwidth = look_distance
+        kernel_bandwidth = estimate_bandwidth(data)/6
         print("Kernel bandwidth: %d" %kernel_bandwidth)
         mean_shift_result = mean_shifter.cluster(reference_points, kernel_bandwidth=kernel_bandwidth)
-        mean_shift_centroids = np.unique(mean_shift_result.shifted_points)
-        print("Cluster ids: ", np.unique(mean_shift_result.cluster_ids))
-        # print("Cluster ids: ", np.unique(mean_shift_result.cluster_ids))
-        
-        # print "Original Point     Shifted Point  Cluster ID"
-        # print "============================================"
-        # for i in range(len(mean_shift_result.shifted_points)):
-        #     original_point = mean_shift_result.original_points[i]
-        #     converged_point = mean_shift_result.shifted_points[i]
-        #     cluster_assignment = mean_shift_result.cluster_ids[i]
-        #     print "(%5.2f,%5.2f)  ->  (%5.2f,%5.2f)  cluster %i" % (original_point[0], original_point[1], converged_point[0], converged_point[1], cluster_assignment)
-
-        # X=ms(data, d,gt)
-        # plot_solution(X,gt)
-        # CI = ci(sol,gt,c)
-        # print("FINISH DATASET %s CI:=%d, MSE=%.2f" % (f, CI, sol.MSE_))
-        # plot_solution(sol,gt)
-
-
+        centroids=get_centroids(mean_shift_result)
+        print("Number of clusters: ", max(mean_shift_result.cluster_ids))
+        CI = ci(centroids,gt,c)
+        MSE = mse(data,centroids, mean_shift_result.cluster_ids)
+        print("FINISH DATASET %s CI:=%d, MSE=%.2f" % (f, CI, MSE))
 
 
 
@@ -90,25 +75,8 @@ look_distance_lightweight =[50000,50000,50000,50000,5000,5000,5000,20000,20]
 k_test = [15]
 files_test = ['s1']
 look_distance_test =[50000]
-# np.random.seed(2991)
 from sys import argv
 if len(argv)==2 and (argv[1] == 'full' or argv[1] == 'test'):
     main(argv[1])
 else:
     main('lightweight')
-
-
-    # reference_points = load_points("data.csv")
-    # mean_shifter = ms.MeanShift()
-    # mean_shift_result = mean_shifter.cluster(reference_points, kernel_bandwidth = 3)
-    
-    # print "Original Point     Shifted Point  Cluster ID"
-    # print "============================================"
-    # for i in range(len(mean_shift_result.shifted_points)):
-    #     original_point = mean_shift_result.original_points[i]
-    #     converged_point = mean_shift_result.shifted_points[i]
-    #     cluster_assignment = mean_shift_result.cluster_ids[i]
-    #     print "(%5.2f,%5.2f)  ->  (%5.2f,%5.2f)  cluster %i" % (original_point[0], original_point[1], converged_point[0], converged_point[1], cluster_assignment)
-
-# if __name__ == '__main__':
-#     run()
--- mean_shift_utils.py
+++ mean_shift_utils.py
@@ -28,3 +28,26 @@ def multivariate_gaussian_kernel(distances, bandwidths):
     val = (1 / np.power((2 * math.pi), (dim/2)) * np.power(np.linalg.det(cov), 0.5)) * np.exp(exponent)
 
     return val
+
+
+
+def get_centroids(ms_result):
+    pts = ms_result.shifted_points
+    lbls = ms_result.cluster_ids
+    n_cens = max(lbls)+1
+    cens = []
+    for i in range(n_cens):
+        part = pts[lbls==i]
+        cens = np.append(cens, np.mean(part,axis=0).tolist(), axis=0)
+    return cens.reshape((n_cens,2))
+
+def mse(data, centroids,lbls):
+    k = max(lbls)+1
+    TSE=0
+    for i in range(k):
+        c = centroids[i]
+        pts = data[lbls==i]
+        for p in pts:
+            e = np.dot(p-c,p-c)
+            TSE+=e
+    return TSE/data.size
