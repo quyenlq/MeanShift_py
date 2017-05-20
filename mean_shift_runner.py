c10747ef96a3cf0d4201ee8d1e07c61a8af6973c
Quốc Quyền Lục <lucquocquyen@gmail.com>
2017-05-20 03:01:20 +0300

    fix kernels

--- mean_shift_runner.py
+++ mean_shift_runner.py
@@ -18,7 +18,10 @@ def ci(cens,gt,k):
     return max(one_way_ci(cens,gt,k),one_way_ci(gt,cens,k))
 
 def one_way_ci(cens_A, cens_B, n_clus):
-    orphans = np.ones(n_clus);
+    sizeA = cens_A.size/2
+    sizeB = cens_B.size/2
+    biggestSize = np.max([sizeA,sizeB,n_clus])
+    orphans = np.ones(biggestSize);
     for cenA in cens_A:
         dist = []
         for cenB in cens_B:
@@ -34,18 +37,18 @@ def main(arg):
         print("Use full dataset, might be slow")
         files=files_full
         k=k_full
-        look_distance = look_distance_full
+        kernel = kernel_full
     elif arg=='test':
         print("Testing algorith, only use S1")
         files=files_test
         k=k_test
-        look_distance = look_distance_test
+        kernel = kernel_test
     else:
         print("Lightweight mode, skip Birch1 and Birch2")
         files = files_lightweight
         k = k_lightweight
-        look_distance = look_distance_lightweight
-    for f,c,d in zip(files,k,look_distance):
+        kernel = kernel_lightweight
+    for f,c,knl in zip(files,k,kernel):
         print 'Running data set %s' %f
         fi = open(f+'.txt')
         fi_gt = open(f+'-gt.txt')
@@ -55,7 +58,7 @@ def main(arg):
         # pdb.set_trace()
         mean_shifter = ms.MeanShift()
         # kernel_bandwidth = look_distance
-        kernel_bandwidth = estimate_bandwidth(data)/6
+        kernel_bandwidth = knl
         print("Kernel bandwidth: %d" %kernel_bandwidth)
         mean_shift_result = mean_shifter.cluster(reference_points, kernel_bandwidth=kernel_bandwidth)
         centroids=get_centroids(mean_shift_result)
@@ -68,15 +71,18 @@ def main(arg):
 
 k_full = [15,15,15,15,20,35,50,8,100,100,16];
 files_full = ['s1','s2','s3','s4', 'a1', 'a2', 'a3','unbalance','birch1', 'birch2','dim32']
-look_distance_full =[50000,50000,50000,50000,5000,5000,5000,20000,5000,5000,20]
+kernel_full =[50000,50000,38000,38000,2000,1000,500,7000,500,500,20]
 k_lightweight = [15,15,15,15,20,35,50,8,16];
 files_lightweight = ['s1','s2','s3','s4', 'a1', 'a2', 'a3','unbalance','dim32']
-look_distance_lightweight =[50000,50000,50000,50000,5000,5000,5000,20000,20]
+kernel_lightweight =[50000,50000,38000,38000,2000,1000,500,7000,20]
 k_test = [15]
 files_test = ['s1']
-look_distance_test =[50000]
+kernel_test =[50000]
 from sys import argv
 if len(argv)==2 and (argv[1] == 'full' or argv[1] == 'test'):
     main(argv[1])
 else:
     main('lightweight')
+
+
+
